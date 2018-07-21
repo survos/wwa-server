49a0e2f5d3313941f8f52d85dfb0ea39706541b1
Tac Tacelosky <tac@survos.com>
2018-07-21 10:15:54 -0400

    add htaccess and database

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Location;
+use App\Entity\Photo;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,6 +11,22 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        for ($i=0; $i<10; $i++) {
+            $photo = new Photo();
+            $photo->setFilename("file" . $i . '.jpg')
+                ->setTitle("Photo $i")
+                ->setDescription("Description of photo $i");
+            $manager->persist($photo);
+        }
+
+        for ($i=0; $i<4; $i++) {
+            $location = new Location();
+            $location->setAddress("$i Main Street")
+                ->setCity("Sperryville")
+                ->setPhoto($photo); // hack, last photo on list!
+            $manager->persist($location);
+        }
+
         // $product = new Product();
         // $manager->persist($product);
 
